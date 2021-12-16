eebf6b759375644772e670a6e4f6f35f65717cf3
Andriy Plekan <andriy05122000@gmail.com>
2021-12-16 19:02:36 +0200

    Update questions.service.ts

--- src/StarForum/client/StarTeam-Forum/src/app/modules/questions/questions.service.ts
+++ src/StarForum/client/StarTeam-Forum/src/app/modules/questions/questions.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ConfigService } from 'src/app/services/config.service';
-import { Config } from 'src/app/types/Config';
 
 @Injectable()
 export class QuestionsService {
